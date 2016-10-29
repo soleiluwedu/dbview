26d33afb877f860991f5b2251dc0b6d8d6de62ff
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-10-28 21:27:00 -0700

    tableController.js updated. createTable not done.

--- src/client/controllers/tableController.js
+++ src/client/controllers/tableController.js
@@ -1,11 +1,22 @@
 angular
   .module('Dbview.TableController', ['ui.router'])
-  .controller('TableController', ['$scope', 'tableService', '$stateParams', 'dbService', tableController])
+  .controller('TableController', ['$scope', 'tableService', '$stateParams', 'dbService', '$http', tableController])
 
-function tableController($scope, tableService, $stateParams, dbService) {
+function tableController($scope, tableService, $stateParams, dbService, $http) {
   $scope.name = $stateParams.tablename;
 
   // reference the data that will be rendered to a table format
   $scope.dataToRender = tableService.getData($stateParams.tablename);
-}
 
+  $scope.deleteRow = () => {
+    $http.post('/delete', { creds: dbService.creds, where: $scope.query })
+      .success((data, status) => { tableService.addTableData($scope.name, data) })
+      .error((data, status) => { console.log("Error:", status) });
+  }
+
+  $scope.createTable = () => {
+    $http.post('/createTable', { creds: dbService.creds, columns: $scope.valuesToInsert })
+      .success((data, status) => { tableService.addTableData($scope.name, data) })
+      .error((data, status) => { console.log("Error:", status) });
+  }
+}
